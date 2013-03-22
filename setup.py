c70ede84cb9e3777f433c613dc687fc92aabc994
Jean-Louis Fuchs <ganwell@fangorn.ch>
2013-03-22 21:52:35 +0100

    * increase version for dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "freeze",
-    version = "0.1.0",
+    version = "0.1.1",
     package_dir = {'': 'lib'},
     py_modules=['freeze'],
 
