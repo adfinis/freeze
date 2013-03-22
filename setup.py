af6b92cd55cb26c31348f3ddb2dccc3b9f5bd702
Jean-Louis Fuchs <ganwell@fangorn.ch>
2013-03-22 16:02:17 +0100

    * complete setup.py settings

--- setup.py
+++ setup.py
@@ -9,10 +9,33 @@ setup(
 
     author = "Jean-Louis Fuchs",
     author_email = "ganwell@fangorn.ch",
-    description = "Freeze - hash / sort / compare / diff anything\n\n"
-                  "Freeze the state of data-structures and objects for "
-                  "data-analysis or testing (diffing data-structures).",
+    description = "Freeze - hash / sort / compare / diff anything",
     license = "Modified BSD",
+    long_description = """
+Freeze the state of data-structures and objects for data-analysis or testing
+(diffing data-structures). Frozen data-structures consist of only tuples
+and these are comparable/sortable/hashable. The freeze method can be used
+for many purposes for example implement __hash__ for your complex object
+very fast. freeze_stable and flatten are usable for testing and analysis.""",
     keywords = "freeze state hash sort compare unittest",
     url = "https://github.com/adfinis-sygroup/freeze",
+    download_url = "https://github.com/adfinis-sygroup/freeze/archive/freeze-0.1.0.tar.gz",
+    #bugtrack_url = "https://github.com/adfinis-sygroup/freeze/issues",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Testing",
+    ]
 )
