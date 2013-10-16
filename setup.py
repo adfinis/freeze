3bbc6343b98d56e8dc3db64a2199eb8261e0d505
Jean-Louis Fuchs <ganwell@fangorn.ch>
2013-10-16 15:50:06 +0200

    * new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "freeze",
-    version = "0.2.0",
+    version = "0.3.0",
     package_dir = {'': 'lib'},
     py_modules=['freeze'],
 
