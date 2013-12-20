9996c728ad35d3b820348f7f2f9508323db8ded8
Jean-Louis Fuchs <ganwell@fangorn.ch>
2013-12-21 00:39:25 +0100

    * cython testing on travis

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 set -e
-nosetests --with-doctest --doctest-tests --cover-package=freeze --with-coverage --cover-tests --cover-erase --cover-min-percentage=100
+if [ "$USE_CYTHON" == "true" ]
+then
+	pip install cython
+	python setup.py build_ext --inplace
+	nosetests --with-doctest --doctest-tests
+else
+	nosetests --with-doctest --doctest-tests --cover-package=freeze --with-coverage --cover-tests --cover-erase --cover-min-percentage=100
+fi
 cd lib
 pylama -l pep8,pyflakes,pylint -i E203,E272,E221,E251,E202,E271,C0302,W0511,F0401
